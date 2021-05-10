3dade2cc690fe80701db775d95d357f8dae1e176
borninmarch93 <vedrana93@gmail.com>
2021-05-11 00:59:02 +0200

    Updates

---
+++ precache-manifest.600b8ab7d4ebc26c37813eb8d31cf428.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "268035828acd4202931fc948a69d8d19",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ed8104b19c2e898e0889",
+    "url": "/static/css/main.6303558b.chunk.css"
+  },
+  {
+    "revision": "d09d34b6c2b9ee4ee142",
+    "url": "/static/js/2.420f534f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.420f534f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed8104b19c2e898e0889",
+    "url": "/static/js/main.1acab65c.chunk.js"
+  },
+  {
+    "revision": "0b7537e113f246ea52f8",
+    "url": "/static/js/runtime-main.24e89aa7.js"
+  }
+]);
